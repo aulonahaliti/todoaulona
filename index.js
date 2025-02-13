1bcc803878b3df6b4ed215420974e45260eaaf3a
Aulona <123652758+aulonahaliti@users.noreply.github.com>
2025-02-13 18:52:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express= require ('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json())
-
+app.use(cors());
 
 //variabla qe ka mi majt tasks
 let todos = [];
